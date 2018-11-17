5cb368819aaa6c3cacdd9d4c59913d397bdc58d2
pjchender <pjchender@gmail.com>
2018-11-17 13:14:11 +0800

    Updates

---
+++ precache-manifest.c14a96d707653dd83b38bc8a297430c4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-router-breadcrumb/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "4d57e6ba8badc114e498",
+    "url": "/react-router-breadcrumb/static/js/runtime~main.4d57e6ba.js"
+  },
+  {
+    "revision": "2db110b7bed2f19296d8",
+    "url": "/react-router-breadcrumb/static/js/main.2db110b7.chunk.js"
+  },
+  {
+    "revision": "a469bd5efd20918b26bc",
+    "url": "/react-router-breadcrumb/static/js/1.a469bd5e.chunk.js"
+  },
+  {
+    "revision": "ce739aec274888af103779364ad0c154",
+    "url": "/react-router-breadcrumb/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d340861b73cd93fb7b8789793d95456e.js"
+  "/react-router-breadcrumb/precache-manifest.c14a96d707653dd83b38bc8a297430c4.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-router-breadcrumb/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
