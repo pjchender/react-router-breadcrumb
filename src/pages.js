951a1b02398a696db0ecf7183608cb28c41c6ec0
pjchender <pjchender@gmail.com>
2018-11-17 08:57:31 +0800

    Create all pages components

---
+++ src/pages.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+/**
+ * These are root pages
+ */
+const Home = () => {
+  return <h1 className="py-3">Home</h1>;
+};
+
+const Books = () => {
+  return <h1 className="py-3">Books</h1>;
+};
+
+const Electronics = () => {
+  return <h1 className="py-3">Electronics</h1>;
+};
+
+/**
+ * These are pages nested in Electronics
+ */
+const Mobile = () => {
+  return <h3>Mobile Phone</h3>;
+};
+
+const Desktop = () => {
+  return <h3>Desktop PC</h3>;
+};
+
+const Laptop = () => {
+  return <h3>Laptop</h3>;
+};
+
+export { Home, Books, Electronics, Mobile, Desktop, Laptop };
